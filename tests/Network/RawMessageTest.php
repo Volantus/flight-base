7197f2f52f3d8e9aa71a3c105ad4fda797f47db5
Marius Meissner <info@marius-meissner.net>
2017-01-14 16:26:05 +0100

    Added RawMessage + factory

---
+++ tests/Network/RawMessageTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Volante\SkyBukkit\RleayServer\Tests\Network;
+
+use Volante\SkyBukkit\Common\Src\Network\RawMessage;
+
+/**
+ * Class MessageTest
+ * @package Volante\SkyBukkit\Monitor\Tests\Message
+ */
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_jsonSerialize()
+    {
+        $expected = [
+            'type' => 'testMessage',
+            'title' => 'This is a test',
+            'data' => [
+                'sub01' => [1, 2, 3],
+                'sub02' => [4, 5, 6]
+            ]
+        ];
+
+        $message = new RawMessage($expected['type'], $expected['title'], $expected['data']);
+        self::assertEquals($expected, $message->jsonSerialize());
+    }
+}
