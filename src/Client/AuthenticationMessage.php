7bf24f85429b6f1fd971fccad212720d90621c93
Marius Meißner <info@marius-meissner.net>
2017-01-14 20:29:20 +0100

    Added AuthenticationMessage

---
+++ src/Client/AuthenticationMessage.php
@@ -0,0 +1,51 @@
+<?php
+namespace Volante\SkyBukkit\Common\Src\Client;
+
+/**
+ * Class AuthenticationMessage
+ * @package Volante\SkyBukkit\Common\Src\Client
+ */
+class AuthenticationMessage extends Message
+{
+    /**
+     * @var string
+     */
+    protected $type = 'authentication';
+
+    /**
+     * @var string
+     */
+    protected $messageTitle = 'Authentication';
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * AuthenticationMessage constructor.
+     * @param string $token
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRawData(): array
+    {
+        return [
+            'token' => $this->token
+        ];
+    }
+}
