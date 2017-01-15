8b6a8a79717cac7e27aa3f47466b16653d5ea035
Marius Meissner <info@marius-meissner.net>
2017-01-15 22:50:09 +0100

    Added tests for GeoPosition

---
+++ tests/General/GeoPosition/GeoPositionTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Volante\SkyBukkit\Common\Tests\General\GeoPosition;
+
+use Volante\SkyBukkit\Common\Src\General\GeoPosition\GeoPosition;
+use Volante\SkyBukkit\Common\Src\General\Network\RawMessage;
+
+/**
+ * Class GeoPositionTest
+ *
+ * @package Volante\SkyBukkit\GeoPositionService\Tests\GeoPosition
+ */
+class GeoPositionTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_toRawMessage_correct()
+    {
+        $expected = [
+            'latitude'  => 30.1001711,
+            'longitude' => 50.7974963,
+            'altitude'  => 512.15
+        ];
+
+        $geoPosition = new GeoPosition($expected['latitude'], $expected['longitude'], $expected['altitude']);
+        $rawMessage = $geoPosition->toRawMessage();
+
+        self::assertInstanceOf(RawMessage::class, $rawMessage);
+        self::assertEquals('Geo position', $rawMessage->getTitle());
+        self::assertEquals('geoPosition', $rawMessage->getType());
+        self::assertEquals($expected, $rawMessage->getData());
+    }
+}
