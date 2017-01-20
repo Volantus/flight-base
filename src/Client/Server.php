972c8a0170c16b695609133e8688ae519b1acee4
Marius Meissner <info@marius-meissner.net>
2017-01-20 20:15:54 +0100

    Renamed role constants of Server and converted them to integers

--- src/Client/Server.php
+++ src/Client/Server.php
@@ -10,9 +10,9 @@ use Volante\SkyBukkit\Common\Src\Server\Messaging\Sender;
  */
 class Server implements Sender
 {
-    const ROLE_LOCAL = 'local';
-    const ROLE_REMOTE = 'remote';
-    const ROLE_GEO_POSITION_SERVICE = 'geoPositionService';
+    const ROLE_LOCAL_RELAY_SERVER   = 1;
+    const ROLE_REMOTE_RELAY_SERVER  = 2;
+    const ROLE_GEO_POSITION_SERVICE = 3;
 
     /**
      * @var WebSocket
