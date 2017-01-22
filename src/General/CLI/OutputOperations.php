6627660c02de42a925289bc82f130eadb01fe3be
Marius Meissner <info@marius-meissner.net>
2017-01-23 00:09:24 +0100

    Added more color to output

--- src/General/CLI/OutputOperations.php
+++ src/General/CLI/OutputOperations.php
@@ -30,7 +30,7 @@ trait OutputOperations
      */
     protected function writeGreenLine(string $topic, string $message)
     {
-        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] [<fg=green>' . $message . '</>]');
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] <fg=green>' . $message . '</>');
     }
 
     /**
@@ -39,7 +39,7 @@ trait OutputOperations
      */
     protected function writeRedLine(string $topic, string $message)
     {
-        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] [<fg=red>' . $message . '</>]');
+        $this->output->writeln('[<fg=blue>' . $this->currentTime() . '</>] [<fg=cyan;options=bold>'. $topic . '</>] <fg=red>' . $message . '</>');
     }
 
     /**
