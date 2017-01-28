1d1059098a403d385e265900d075ebd4d4d38fb9
Marius Meissner <info@marius-meissner.net>
2017-01-28 23:30:03 +0100

    Added GyroStatus

---
+++ tests/General/GyroStatus/GyroStatusTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Volante\SkyBukkit\Common\Tests\General\GyroStatus;
+
+use Volante\SkyBukkit\Common\Src\General\GyroStatus\GyroStatus;
+use Volante\SkyBukkit\Common\Src\General\Network\BaseRawMessage;
+
+/**
+ * Class GyroStatusTest
+ *
+ * @package Volante\SkyBukkit\Common\Tests\General\GyroStatus
+ */
+class GyroStatusTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_toRawMessage_correct()
+    {
+        $expected = [
+            'yaw'   => 1.11,
+            'pitch' => 2.22,
+            'roll'  => 3.33
+        ];
+        $gyroStatus = new GyroStatus(1.11, 3.33, 2.22);
+        $result = $gyroStatus->toRawMessage();
+
+        self::assertInstanceOf(BaseRawMessage::class, $result);
+        self::assertEquals('gyroStatus', $result->getType());
+        self::assertEquals('Gyro Status', $result->getTitle());
+        self::assertEquals($expected, $result->getData());
+    }
+}
