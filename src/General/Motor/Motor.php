1624f9c11bb70f983f3d5370a1f9742aa46a85e1
Marius Meißner <info@marius-meissner.net>
2017-02-04 14:18:10 +0100

    Added Motor

---
+++ src/General/Motor/Motor.php
@@ -0,0 +1,108 @@
+<?php
+namespace Volante\SkyBukkit\Common\Src\General\Motor;
+
+/**
+ * Class Motor
+ * @package Volante\SkyBukkit\FlightController\Src\FlightController
+ */
+class Motor implements \JsonSerializable
+{
+    const ZERO_LEVEL = 1000;
+    const FULL_LEVEL = 2000;
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $gpioPin;
+
+    /**
+     * @var int
+     */
+    private $power;
+
+    /**
+     * Motor constructor.
+     *
+     * @param int $id
+     * @param int $power
+     * @param int $gpioPin
+     */
+    public function __construct(int $id, int $power, int $gpioPin = null)
+    {
+        $this->id = $id;
+        $this->power = $power;
+
+        $this->gpioPin = $gpioPin ?: (int) getenv('MOTOR_' . $this->id . '_PIN');
+        if (!is_int($this->gpioPin) || $this->gpioPin == 0) {
+            throw new \InvalidArgumentException('GPIO Pin for motor ' . $this->id . ' needs to be configured');
+        }
+    }
+
+    /**
+     * @param int $delta
+     */
+    public function changePower(int $delta)
+    {
+        $this->power += $delta;
+        $this->keepPowerLimits();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPower(): float
+    {
+        return $this->power;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param int $power
+     */
+    public function setPower(int $power)
+    {
+        $this->power = $power;
+        $this->keepPowerLimits();
+    }
+
+    private function keepPowerLimits()
+    {
+        if ($this->power < self::ZERO_LEVEL) {
+            $this->power = self::ZERO_LEVEL;
+        } elseif ($this->power > self::FULL_LEVEL) {
+            $this->power = self::FULL_LEVEL;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function jsonSerialize()
+    {
+        return [
+            'id'    => $this->id,
+            'pin'   => $this->gpioPin,
+            'power' => $this->power
+        ];
+    }
+}
---
+++ tests/General/Motor/MotorTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace  Volante\SkyBukkit\Common\Tests\General\Motor\FlightControllerTest;
+
+use Volante\SkyBukkit\Common\Src\General\Motor\Motor;
+
+/**
+ * Class MotorTest
+ *
+ * @package Volante\SkyBukkit\FlightController\Tests\FlightControllerTest
+ */
+class MotorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage GPIO Pin for motor 9 needs to be configured
+     */
+    public function test_constructor_gpioPinNotConfigured()
+    {
+        putenv('MOTOR_9_PIN');
+        new Motor(9, Motor::ZERO_LEVEL);
+    }
+
+    public function test_jsonSerialize_correct()
+    {
+        $expected = [
+            'id'    => 5,
+            'pin'   => 22,
+            'power' => Motor::ZERO_LEVEL + 200
+        ];
+        $motor = new Motor(5, $expected['power'], 22);
+
+        self::assertEquals($expected, $motor->jsonSerialize());
+    }
+
+    public function test_changePower_positiveChange()
+    {
+        $motor = new Motor(0, Motor::ZERO_LEVEL, 1);
+        $motor->changePower(200);
+
+        self::assertEquals(Motor::ZERO_LEVEL + 200, $motor->getPower());
+    }
+
+    public function test_changePower_negativeChange()
+    {
+        $motor = new Motor(0, Motor::FULL_LEVEL, 1);
+        $motor->changePower(-200);
+
+        self::assertEquals(Motor::FULL_LEVEL - 200, $motor->getPower());
+    }
+
+    public function test_changePower_minLimit()
+    {
+        $motor = new Motor(0, Motor::ZERO_LEVEL, 1);
+        $motor->changePower(-200);
+
+        self::assertEquals(Motor::ZERO_LEVEL, $motor->getPower());
+    }
+
+    public function test_changePower_maxLimit()
+    {
+        $motor = new Motor(0, Motor::FULL_LEVEL, 1);
+        $motor->changePower(200);
+
+        self::assertEquals(Motor::FULL_LEVEL, $motor->getPower());
+    }
+
+    public function test_setPower_correct()
+    {
+        $motor = new Motor(0, Motor::ZERO_LEVEL, 1);
+        $motor->setPower(Motor::ZERO_LEVEL + 200);
+
+        self::assertEquals(Motor::ZERO_LEVEL + 200, $motor->getPower());
+    }
+
+    public function test_setPower_minLimit()
+    {
+        $motor = new Motor(0, Motor::ZERO_LEVEL, 1);
+        $motor->setPower(Motor::ZERO_LEVEL - 200);
+
+        self::assertEquals(Motor::ZERO_LEVEL, $motor->getPower());
+    }
+
+    public function test_setPower_maxLimit()
+    {
+        $motor = new Motor(0, Motor::FULL_LEVEL, 1);
+        $motor->setPower(Motor::FULL_LEVEL + 200);
+
+        self::assertEquals(Motor::FULL_LEVEL, $motor->getPower());
+    }
+
+    protected function tearDown()
+    {
+        putenv('MOTOR_9_PIN=1');
+    }
+}
