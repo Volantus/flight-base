48af8a17126556e6bdee1334f0d1ea15e469c21c
Marius Meißner <info@marius-meissner.net>
2017-02-04 15:19:31 +0100

    Added motor status + factory

---
+++ tests/Client/OutgoingMessageTestCase.php
@@ -0,0 +1,11 @@
+<?php
+namespace Volante\SkyBukkit\Common\Tests\Client;
+
+/**
+ * Class OutgoingMessageTestCase
+ * @package Volante\SkyBukkit\Common\Tests\Client
+ */
+abstract class OutgoingMessageTestCase
+{
+
+}
