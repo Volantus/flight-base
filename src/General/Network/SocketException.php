c753cdfd8cbdc614d9dc99cb064eb1e40528b657
Marius Meißner <info@marius-meissner.net>
2017-02-21 22:39:38 +0100

    Added socket wrapper

---
+++ src/General/Network/SocketException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Volante\SkyBukkit\Common\Src\General\Network;
+
+/**
+ * Class SocketException
+ *
+ * @package Volante\SkyBukkit\Common\Src\General\Network
+ */
+class SocketException extends \RuntimeException
+{
+}
