3b81283ece51cd5c505d24b48982ca9fdfe83955
Marius Meißner <info@marius-meissner.net>
2017-06-02 17:48:52 +0200

    Added client role ORIENTATION_CONTROL_SERVICE

--- src/General/Role/ClientRole.php
+++ src/General/Role/ClientRole.php
@@ -7,17 +7,18 @@ namespace Volantus\FlightBase\Src\General\Role;
  */
 abstract class ClientRole
 {
-    const OPERATOR               = 1;
-    const MANUAL_CONTROL_SERVICE = 2;
-    const GYRO_STATUS_SERVICE    = 3;
-    const MOTOR_STATUS_SERVICE   = 4;
+    const OPERATOR                    = 1;
+    const MANUAL_CONTROL_SERVICE      = 2;
+    const GYRO_STATUS_SERVICE         = 3;
+    const MOTOR_STATUS_SERVICE        = 4;
+    const ORIENTATION_CONTROL_SERVICE = 5;
 
     /**
      * @return array
      */
     public static function getSupportedRoles(): array
     {
-        return [self::OPERATOR, self::MANUAL_CONTROL_SERVICE, self::GYRO_STATUS_SERVICE, self::MOTOR_STATUS_SERVICE];
+        return [self::OPERATOR, self::MANUAL_CONTROL_SERVICE, self::GYRO_STATUS_SERVICE, self::MOTOR_STATUS_SERVICE, self::ORIENTATION_CONTROL_SERVICE];
     }
 
     /**
@@ -38,6 +39,9 @@ abstract class ClientRole
                 break;
             case self::MOTOR_STATUS_SERVICE:
                 return 'MOTOR_STATUS_SERVICE';
+                break;
+            case self::ORIENTATION_CONTROL_SERVICE:
+                return 'ORIENTATION_CONTROL_SERVICE';
                 break;
         }
 
